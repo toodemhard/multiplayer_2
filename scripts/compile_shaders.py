b3185b1f9f8d1eb488e172a5f3c31184eefae171
toodemhard <lailokkay@gmail.com>
2024-11-14 15:56:39 +0000

    Add scripts and shaders

---
+++ scripts/compile_shaders.py
@@ -0,0 +1,23 @@
+import os
+import subprocess
+from pathlib import Path
+
+files = os.listdir("./shaders")
+
+shader_out_dir = Path("data/shaders/")
+shader_out_dir.mkdir(exist_ok=True, parents=True)
+for src in files:
+    subprocess.run(["glslc", f"./shaders/{src}", "-o", shader_out_dir / f"{src}.spv"])
+
+header_file_path = Path("src/codegen/shaders.h")
+header_file_path.parent.mkdir(exist_ok=True, parents=True)
+with open(header_file_path, "w+") as header:
+    header.write("""#pragma once
+
+namespace shaders {
+""")
+    for src in files:
+        var_name = src.replace(".", "_")
+        header.write(f"    constexpr const char* {var_name} = \"shaders/{src}.spv\";\n")
+
+    header.write("}")
---
+++ scripts/define_assets.py
@@ -0,0 +1,22 @@
+import os
+import subprocess
+from pathlib import Path
+
+asset_files = os.listdir("./data/assets")
+
+header_file_path = Path("src/codegen/assets.h")
+header_file_path.parent.mkdir(exist_ok=True, parents=True)
+with open(header_file_path, "w+") as header:
+    header.write("""#pragma once
+
+namespace assets {
+""")
+
+    for file in asset_files:
+        # var_name = Path(file).stem
+        var_name = file.replace(".", "_")
+        var_name = var_name.replace(" ", "_")
+
+        header.write(f"    constexpr const char* {var_name} = \"assets/{file}\";\n")
+
+    header.write("}")
