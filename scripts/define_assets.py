dc4edee5ac3dc861fdb2f1667f8c77d94d9a3a28
Leqi <100080774+toodemhard@users.noreply.github.com>
2024-11-30 21:31:47 +0000

    forgot this

--- scripts/define_assets.py
+++ scripts/define_assets.py
@@ -3,21 +3,24 @@ import subprocess
 from pathlib import Path
 import base_path
 
-asset_files = os.listdir("./data/assets")
+def write_asset_table(source_dir, file_name):
+    asset_files = os.listdir(f"./data/{source_dir}")
 
-header_file_path = base_path.base_path / "assets.h"
-header_file_path.parent.mkdir(exist_ok=True, parents=True)
-with open(header_file_path, "w+") as header:
-    header.write("""#pragma once
+    header_file_path = base_path.base_path / f"{file_name}.h"
+    header_file_path.parent.mkdir(exist_ok=True, parents=True)
 
-namespace assets {
+    with open(header_file_path, "w+") as header:
+        header.write(f"""#pragma once
+
+#define {file_name.upper()}(_)\\
 """)
 
-    for file in asset_files:
-        # var_name = Path(file).stem
-        var_name = file.replace(".", "_")
-        var_name = var_name.replace(" ", "_")
+        for file in asset_files:
+            # var_name = Path(file).stem
+            var_name = file.replace(".", "_")
+            var_name = var_name.replace(" ", "_")
 
-        header.write(f"    constexpr const char* {var_name} = \"assets/{file}\";\n")
+            header.write(f"    _({var_name}, \"{source_dir}/{file}\")\\\n")
 
-    header.write("}")
+write_asset_table("images", "image_table");
+write_asset_table("fonts", "font_table");
