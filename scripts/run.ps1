1ae7ab4d9c85d0d9bd94ae334ceba79b4db5f38b
Leqi <100080774+toodemhard@users.noreply.github.com>
2024-11-30 21:32:10 +0000

    run target

--- scripts/run.ps1
+++ scripts/run.ps1
@@ -1,9 +1,9 @@
-cmake --build .
+cmake --build . --target $args[0]
 
 if($LASTEXITCODE -eq 0) {
     Write-Host "success"
 
-    ./idk.exe
+    Invoke-Expression "./$($args[0]).exe"
 } else {
     Write-Host "failure"
 }
