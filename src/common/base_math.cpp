a4605ae6fbab1c07780b86de30e53ec0b60e93ac
Leqi <100080774+toodemhard@users.noreply.github.com>
2025-02-23 20:00:28 +0000

    vector + matrix stuff

---
+++ src/common/base_math.cpp
@@ -0,0 +1,74 @@
+#include "../pch.h"
+#include "common/base_math.h"
+
+float2 operator+(const float2& a, const float2& b) {
+    float2 c;
+    for (u32 i = 0; i < 2; i++) {
+        c.v[i] = a.v[i] + b.v[i];
+    }
+    return c;
+}
+
+float2 operator-(const float2& a, const float2& b) {
+    float2 c;
+    for (u32 i = 0; i < 2; i++) {
+        c.v[i] = a.v[i] - b.v[i];
+    }
+    return c;
+}
+
+float2 operator/(const float2& a, const float2& b) {
+    float2 c;
+    for (u32 i = 0; i < 2; i++) {
+        c.v[i] = a.v[i] / b.v[i];
+    }
+    return c;
+}
+
+float2 operator*(const float2& a, const float2& b) {
+    float2 c;
+    for (u32 i = 0; i < 2; i++) {
+        c.v[i] = a.v[i] * b.v[i];
+    }
+    return c;
+}
+
+float2 operator*(float a, const float2& b) {
+    float2 c;
+    for (u32 i = 0; i < 2; i++) {
+        c.v[i] = a * b.v[i];
+    }
+    return c;
+}
+
+float2 operator*(const float2& a, float b) {
+    return b * a;
+}
+
+float2 operator/(const float2& a, float b) {
+    float2 c;
+    for (u32 i = 0; i < 2; i++) {
+        c.v[i] = a.v[i] / b;
+    }
+    return c;
+}
+
+float2 operator*(const float2x2& a, const float2& b) {
+    float2 c;
+    for (u32 j = 0; j < 2; j++) {
+        float v = 0;
+        for (u32 i = 0; i < 2; i++) {
+            v += a.v[j][i] * b.v[i];
+        }
+        c.v[j] = v;
+    }
+    return c;
+}
+
+float magnitude(const float2& a) {
+    return sqrtf(a.x * a.x + a.y * a.y);
+}
+
+float2 normalize(const float2& a) {
+    return a / magnitude(a);
+}
---
+++ src/common/base_math.h
@@ -0,0 +1,50 @@
+#pragma once
+#include "types.h"
+
+union float4 {
+    struct {
+        f32 r, g, b, a;
+    };
+    f32 v[4];
+};
+
+union float2 {
+    struct {
+        f32 x;
+        f32 y;
+    };
+    f32 v[2];
+    b2Vec2 b2vec;
+};
+
+struct float2x2 {
+    f32 v[2][2];
+};
+
+constexpr float2 float2_one = {1,1};
+
+union Rect {
+    struct {
+        float2 position;
+        float2 size;
+    };
+    struct {
+        f32 x;
+        f32 y;
+        f32 w;
+        f32 h;
+    };
+};
+
+float2 operator+(const float2& a, const float2& b);
+float2 operator-(const float2& a, const float2& b);
+float2 operator/(const float2& a, const float2& b);
+float2 operator*(const float2& a, const float2& b);
+float2 operator*(float a, const float2& b);
+float2 operator/(const float2& a, float b);
+float2 operator*(const float2& a, float b);
+
+float2 operator*(const float2x2& a, const float2& b);
+
+float magnitude(const float2& a);
+float2 normalize(const float2& a);
